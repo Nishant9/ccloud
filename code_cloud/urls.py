9b644efc19af6e314f06254806405929334e1b69
Nishant9 <nishant.gupta291995@gmail.com>
2016-03-07 19:29:43 +0530

    Front-end : Complete Main page layout.

--- code_cloud/urls.py
+++ code_cloud/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import url, include
 from django.contrib.auth import views as auth_views
-
+from django.views.generic import TemplateView
 from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^accounts/login/$', auth_views.login, {'redirect_field_name': '/success/'}),
     url(r'^logout',views.logout_view, name='logged out'),
-    url(r'^success/$', views.index)
+    url(r'^success/$', views.index),
+    url(r'^tags/', TemplateView.as_view(template_name="index_tags.html"),name='tags'),
+    url(r'^all_problems/', TemplateView.as_view(template_name="index_all_problems.html"),name='all_problems')
 ]
